d4bd26e27c2c7cf23065856f6c1f71311e269b47
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-08-04 17:10:13 -0700

    navigation structure part1 finished

--- portfolio/src/components/dashboard/Dashboard.js
+++ portfolio/src/components/dashboard/Dashboard.js
@@ -4,11 +4,12 @@ import React from 'react';
 import Navigation from './Navigation';
 
 // Styles
-import {AppContainer} from '../../styles/dashboard/Dashboard.js'
+import {AppContainer, ContentContainer} from '../../styles/dashboard/Dashboard.js'
 const Dashboard = () => {
     return (
         <AppContainer>
             <Navigation />
+            <ContentContainer></ContentContainer>
         </AppContainer>
     )
 
--- portfolio/src/styles/dashboard/Dashboard.js
+++ portfolio/src/styles/dashboard/Dashboard.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
+    overflow: visible;
     width: 100vw;
     height: 100vh;
-    background: black;
+    background: white;
     font-family: Inter, sans-serif;
     font-weight: 400;
     line-height: 1.2;
+`;
+
+export const ContentContainer = styled.div`
+    border: 1px solid blue;
+    height: 95.5vh;
+    width: 100vw;
 `;
