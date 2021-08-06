397ee6899118f8cdf57ca4555e5adad4cc27fb45
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-08-05 22:24:59 -0700

    adding welcome content

---
+++ portfolio/src/components/dashboard/content/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return(
+        <div id="welcome-container">
+            <div id="welcome-text-container">welcome</div>
+        </div>
+    )
+};
+
+export default Welcome;
