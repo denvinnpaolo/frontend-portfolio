7ea54482e994904182ecfb42f1eeb2689cc9aefa
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-08-09 14:42:01 -0700

    welcome information fized

--- portfolio/src/components/dashboard/content/Welcome.js
+++ portfolio/src/components/dashboard/content/Welcome.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Typewriter from 'typewriter-effect';
 const Welcome = () => {
 
-    const title = ['Software Engineer', 'Problem Solver']
+    const title = ['Software Engineer', 'Problem Solver', ".NET Developer"]
 
     const Writer = props => {
         return(
             <Typewriter 
-            options={{
-                strings: props.title,
-                autoStart: true,
-                loop: true,}}
-                />
+                options={
+                    {strings: props.title,
+                    autoStart: true,
+                    loop: true}
+                }
+            />
         )
     }
     return(
@@ -20,8 +21,9 @@ const Welcome = () => {
                 <h1 id="welcome-title">Denvinn P. Magsino</h1>
                 <h1 id="welcome-title"><Writer title={title} /></h1>
                 <div id="welcome-subtitle">Use AI to write proven, high converting copy for increased conversions and higher ROI. Instantly generate high-quality copy for ads, emails, websites, listings, blogs, and more...</div>
-                <a id="welcome-btn">Get started →</a>
+                <a id="welcome-btn">About →</a>
             </div>
+            <div id="welcome-slider"> HELLO</div>
         </div>
     )
 };
