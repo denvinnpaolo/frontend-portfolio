9330a3a9f43a9bb707b92e8557a6aa54e59060ad
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-09-19 20:52:01 -0700

    adding extra blog post

--- portfolio/src/components/dashboard/Dashboard.js
+++ portfolio/src/components/dashboard/Dashboard.js
@@ -17,6 +17,7 @@ const Dashboard = () => {
                 <Welcome />
                 <Top />
                 <BlogPost />
+                <BlogPost />
             </ContentContainer>
         </AppContainer>
     )
